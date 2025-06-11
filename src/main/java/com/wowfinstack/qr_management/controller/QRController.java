7c5b936f0405cdc63e368c28894e19e04eb9e16b
srishti-rao <srishtirao7@gmail.com>
2025-06-11 16:16:29 +0545

    feature(swagger): add annotation for comprehensive swagger documentation

--- src/main/java/com/wowfinstack/qr_management/controller/QRController.java
+++ src/main/java/com/wowfinstack/qr_management/controller/QRController.java
@@ -2,6 +2,8 @@ package com.wowfinstack.qr_management.controller;
 
 import com.wowfinstack.qr_management.dto.request.QRRequest;
 import com.wowfinstack.qr_management.service.QRService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/qrcode")
 @RequiredArgsConstructor
+@Tag(name = "QR", description = "APIs related to managing QR")
 public class QRController {
     private final QRService qrService;
 
+    @Operation(summary = "Generate QR", description = "Generates QR with company logo.")
     @PostMapping(value = "/generate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<byte[]> generateQRCode(@Valid @RequestBody QRRequest qrRequest) {
         byte[] qrCode = qrService.generateQRCode(qrRequest.getContent());
--- src/main/java/com/wowfinstack/qr_management/dto/request/QRRequest.java
+++ src/main/java/com/wowfinstack/qr_management/dto/request/QRRequest.java
@@ -1,10 +1,14 @@
 package com.wowfinstack.qr_management.dto.request;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class QRRequest {
     @NotBlank(message = "QR Content should not be blank")
+    @Schema(name = "content", example = "1234567890", requiredMode = Schema.RequiredMode.REQUIRED)
     private String content;
 }
